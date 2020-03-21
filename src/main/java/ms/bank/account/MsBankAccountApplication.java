a5f8025f4e25d82098eef8d3e5a2adbf86afc3e4
Cesar Gomez <cesar.gomez@urp.edu.pe>
2020-03-20 21:24:51 -0500

    Project created

---
+++ src/main/java/ms/bank/account/MsBankAccountApplication.java
@@ -0,0 +1,13 @@
+package ms.bank.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBankAccountApplication.class, args);
+	}
+
+}
