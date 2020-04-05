8b3699dd97c540ff8b7ab642ff1eaba5473d9366
Cesar Gomez <cesar.gomez@urp.edu.pe>
2020-04-04 23:31:29 -0500

    Eureka Client Configuration

--- src/test/java/ms/bank/account/MsBankAccountApplicationTests.java
+++ src/test/java/ms/bank/account/MsBankAccountApplicationTests.java
@@ -2,12 +2,14 @@ package ms.bank.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootTest
 class MsBankAccountApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
